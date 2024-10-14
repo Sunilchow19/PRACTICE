9efc086ee20d09381000ec5ddbda73cd878ca821
chaitukumar15 <108114098+chaitukumar15@users.noreply.github.com>
2024-10-14 15:21:08 +0530

    changes

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ var express= require("express");
 
 var app=express();
 
+var mysql2= require("mysql2");
+
+
+var conn=mysql2.createConnection({
+
+    host:"localhost",
+    user:"root",
+    password:"chaitu1504",
+    database:"dummy"
+})
+
+conn.connect((err)=>{
+    if(err){
+        console.log(err);
+        
+    }else{
+        console.log("conected to mysql db");
+        
+    }
+})
+
+
 app.get("/",(req,res)=>{
 
 
